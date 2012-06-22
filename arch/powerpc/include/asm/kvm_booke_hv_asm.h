8912c7eca3637158a54dbeccca61c9c7e1c40af9
Mihai Caraman <mihai.caraman@freescale.com>
2012-06-23 01:20:20 +0300

    KVM: PPC64: bookehv: Add support for interrupt handling
    Add bookehv interrupt handling support for 64-bit hosts. Change common stack
    layout to refer PPC_LR_STKOFF kernel constant. Dispatch the 64-bit execution
    flow to the existing kvm_handler_common asm macro. Update input register
    values documentation.
    Only the bolted version of TLB miss exception handlers is supported now.
    
    Signed-off-by: Mihai Caraman <mihai.caraman@freescale.com>

--- arch/powerpc/include/asm/kvm_booke_hv_asm.h
+++ arch/powerpc/include/asm/kvm_booke_hv_asm.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2011 Freescale Semiconductor, Inc.
+ * Copyright 2010-2012 Freescale Semiconductor, Inc.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License, version 2, as
@@ -17,6 +17,7 @@
  * there are no exceptions for which we fall through directly to
  * the normal host handler.
  *
+ * 32-bit host
  * Expected inputs (normal exceptions):
  *   SCRATCH0 = saved r10
  *   r10 = thread struct
@@ -33,6 +34,15 @@
  *   *(r8 + GPR9) = saved r9
  *   *(r8 + GPR10) = saved r10 (r10 not yet clobbered)
  *   *(r8 + GPR11) = saved r11
+ *
+ * 64-bit host
+ * Expected inputs (exception types GEN/DBG/CRIT/MC):
+ *  r13 = PACA_POINTER
+ *  r10 = saved CR
+ *  SPRN_SPRG_##type##_SCRATCH = saved r13
+ *  *(r13 + PACA_EX##type + EX_R10) = saved r10
+ *  *(r13 + PACA_EX##type + EX_R11) = saved r11
+ * Only the bolted version of TLB miss exception handlers is supported now.
  */
 .macro DO_KVM intno srr1
 #ifdef CONFIG_KVM_BOOKE_HV
